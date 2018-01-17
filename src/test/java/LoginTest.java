04ff3db8e401966edbd6de381f82c74cb5a77532
myselfganu <myselfganu@users.noreply.github.com>
2018-01-17 11:04:21 +0530

    Modified LoginTest.java file
    running the complete feature file

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = "src/test/java/in/raster/cucumber/features/" ,
-                    tags = "@loginFalseFail",
+                    tags = "@loginFeature",
                     format = {"pretty", "html:target/cucumber", "json:target/cucumber-report.json"})
 public class LoginTest {
 }
